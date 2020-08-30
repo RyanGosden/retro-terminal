ae04dbfd098c7dc84ee219f717699eac9259bfd0
Ryan Gosden <ryangosden@gmail.com>
2020-08-30 14:49:14 +0200

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,101 @@
-import React from "react"
+import React, { useState } from 'react';
+import './styles.css';
 
 export default function Home() {
-  return <div>Hello world!</div>
+
+  const [prompt, setPrompt] = useState("");
+  const [output, setOutput] = useState([]);
+  const [commands, setCommands] = useState([]);
+
+  // Seek Position
+  let [seekPosition, setSeekPosition] = useState(0);
+
+  // PromptPrefix
+  const [user, setUser] = useState("user@solaris");
+  const [path, setPath] = useState("/");
+  const [userType, setUserType] = useState("$");
+
+  const parseCommand = (prompt) => {
+
+    // Add output
+    const newOutput = [...output, ...[{prompt: prompt, orig: 'user'}] ];
+
+    switch (prompt) {
+      case 'clear' :
+        setOutput([]);
+        break;
+
+      default:
+        setOutput([...newOutput, ...[{prompt: `Command '${prompt}' not found`, orig: 'system'}]]);
+    }
+  }
+
+  const handleEvents = (e) => {
+    // Enter
+    if (e.keyCode === 13) {
+      // Add command to history
+      const newCommands = [...commands, prompt];
+      setCommands(newCommands);
+
+      setPrompt('');
+      setSeekPosition(0);
+
+      parseCommand(prompt);
+    }
+
+    // Up Arrow
+    if (e.keyCode === 38) {
+      if (commands.length <= 0 || (seekPosition === commands.length)    )  {
+        return;
+      }
+
+      // Set
+      setSeekPosition(++seekPosition);
+      setPrompt([...commands][commands.length - seekPosition]);
+    }
+
+    // Down Arrow
+    if (e.keyCode === 40) {
+      if (commands.length <= 0 || (seekPosition <= 1)    )  {
+        return;
+      }
+
+      setSeekPosition(--seekPosition);
+      setPrompt([...commands][commands.length - seekPosition]);
+    }
+
+  };
+
+  const handleInput = (e) => setPrompt(e.target.value);
+
+  const promptPrefix = () => {
+    return (
+      <React.Fragment>
+        <span>{user}</span>
+        <span>:</span>
+        <span>{path}</span>
+        <span>{userType}</span>
+      </React.Fragment>
+  )
+  }
+
+  return (
+    <div className="screen">
+        <div className="output">
+          {output.map((out, key) => {
+            return <div key={key} className="output_prompt">
+              {out.orig === 'user' ? promptPrefix() : null}
+              {out.prompt}</div>
+          })
+          }
+        </div>
+
+     <div className="input">
+      <label className="user">
+        {promptPrefix()}
+        <input type="text" value={prompt} className="prompt" onChange={handleInput} onKeyDown={handleEvents} autoFocus/>
+      </label>
+     </div>
+    </div>
+  )
 }
