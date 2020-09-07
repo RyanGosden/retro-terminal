43c3557bed7cbbd781cfac7a2380fbe34d4a3aec
Ryan Gosden <ryangosden@gmail.com>
2020-09-07 22:14:28 +0200

    extension change2

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,7 +98,7 @@ export default function Home() {
   // Load file to view
   const loadFile = file => {
     axios
-      .get(`static/${file}.txt`)
+      .get(`${file}.txt`)
       .then(response => {
         const lines = response.data.split("\n")
 
